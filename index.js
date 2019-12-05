2b7d31b1c3a5644c65274607457a92ed5d8af9f8
Vladyslav-K <vladyslav.kliuiev@gmail.com>
2019-12-05 18:58:08 +0200

    feat: add basic routes to the project
    Created routes for orders, users and projects
    with approximate basic functionality.

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const express = require("express");
 const app = express();
 const port = 3005;
 
-app.get("/", (req, res) => {
-  // Get some data from request:
-  console.log("REQUEST", req.query);
+const products = require("./routes/products");
+const orders = require("./routes/orders");
+const users = require("./routes/users");
 
-  // Modify headers:
+app.use("/products", products);
+app.use("/orders", orders);
+app.use("/users", users);
+
+app.get("/", (req, res) => {
   res.header("Cache-Control", "no-cache, no-store, must-revalidate");
   res.header("Content-Type", "application/json");
 
-  // Send response:
   res.send({ message: "hello" });
 });
 
-// Start app:
 app.listen(port, () => console.log(`API listening on port ${port}!`));
